5be6e900f444fb88e24f5f57e58bc4aa5a080e9e
minhashaik <minha.shaik.s82@kalvium.community>
2025-05-12 01:13:42 +0530

    done

--- backend/models/JournalEntry.js
+++ backend/models/JournalEntry.js
@@ -1,5 +1,5 @@
 const mongoose = require("mongoose");
-
+// Define the schema for a journal entry
 const journalEntrySchema = new mongoose.Schema({
   routine: { type: mongoose.Schema.Types.ObjectId, ref: "Routine", required: true },
   content: { type: String, required: true }
